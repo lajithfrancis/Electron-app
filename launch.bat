24e599df02c5325d448f1815d7554d148a0e66ad
lajithfrancis <lajith.francis@perfomatix.com>
2024-03-14 07:17:48 +0530

    Update paths to use relative paths instead of hardcoded paths.

--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
 @echo off
 FOR /F "tokens=5" %%a IN ('netstat -aon ^| find "3000"') DO taskkill /F /PID %%~nxa
-cd /d "D:\Projects\Electron-app\standalone"
+cd /d "%~dp0standalone"
 start "" cmd /c "node server.js"
-start "" "D:\Projects\Electron-app\output\My Electron App Setup 1.0.0.exe"
+start "" "%~dp0output\My Electron App Setup 1.0.0.exe"
 pause
